3188a91fafd7d15e154b14d20872250d7559eaaf
shaggy0798 <david.freer@gmail.com>
2017-07-19 17:06:25 -0400

    Rectangles 9.6 (unfinished).

---
+++ src/RectangleTester96.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+/**
+ * Created by dfreer on 7/19/2017.
+ */
+interface Measurer{
+    double measure(Object anObject);
+}
+
+public class RectangleTester96 implements Measurer{
+    public double measure(Object anObject){
+        Rectangle rect = (anObject)
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
